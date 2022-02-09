f9137bffd0a530bc04e82dd51fdc6d30e74ebca2
DevelopJKong <ybaram40@gmail.com>
2022-02-09 11:29:31 +0900

    #7.5 React Router 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
- return <Home/>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/movie">
+            <Detail />
+        </Route>
+
+        <Route path="/">
+            <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,11 +1,13 @@
 import PropTypes from "prop-types";
-
+import { Link } from "react-router-dom";
 
 function Movie({ medium_cover_image, title, summary, genres }) {
   return (
     <div>
       <img src={medium_cover_image} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -22,6 +24,5 @@ Movie.propTypes = {
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-
 
 export default Movie;
