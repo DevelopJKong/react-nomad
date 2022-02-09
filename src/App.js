c3c5c4b754746b9311ef0a9424317c991aa90261
DevelopJKong <ybaram40@gmail.com>
2022-02-09 11:36:05 +0900

    coin-project 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import Home from "./routes/Home";
+import { useState, useEffect } from "react";
 
 function App() {
- return <Home/>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((res) => res.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading....</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li>
+              {coin.name} ({coin.symbol}):{coin.quotes.USD.price} USD
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
